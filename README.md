f458b1044021da45871a184f738521c5a2230fc2
ev1313 <tim_blume@web.de>
2013-07-11 12:54:23 +0200

    added a second sample, sample 1 no longer uses the renderer, but a sdl opengl context.

---
+++ README.md
@@ -0,0 +1,9 @@
+Pascal-SDL-2-Samples
+====================
+
+This are Samples for SDL 2.0.
+
+Sample_1:
+---------
+
+This Sample demonstrates how initializing easy an window with opengl-support and the corresponding renderer.
--- sample_2/sample_2.dpr
+++ sample_2/sample_2.dpr
@@ -2,8 +2,6 @@ program sample_1;
 
 {$APPTYPE CONSOLE}
 
-{$R *.res}
-
 {$IFDEF FPC}
   {$MODE DELPHI}
 {$ENDIF}
@@ -15,15 +13,12 @@ uses
 
 var
   window: PSDL_Window;
+  window2: PSDL_Window;
   context: TSDL_GLContext;
   running: Boolean;
 
 procedure OpenGL;
 begin
-  InitOpenGL;
-  ReadExtensions;
-  ReadImplementationProperties;
-
   glClearColor(0.0, 0.0, 0.0, 1.0);
   glClearDepth(1.0);
   glEnable(GL_DEPTH_TEST);
@@ -37,7 +32,7 @@ begin
   if h <= 0 then h := 1;
 
   glViewport( 0, 0, w, h );
- 
+
   glMatrixMode( GL_PROJECTION );
     glLoadIdentity;
     gluPerspective( 45.0, w / h, 0.1, 100.0 );
@@ -53,22 +48,36 @@ begin
   while SDL_PollEvent(@event) > 0 do
   begin
     case event.type_ of
-      //only works with one window!
-      //see sample_2 how to catch this event correct.
-      SDL_QUITEV:
-        running := false;
+      SDL_WINDOWEVENT:
+      begin
+        case event.window.event of
+          SDL_WINDOWEVENT_CLOSE:
+          begin
+            if event.window.windowID = SDL_GetWindowID(window) then
+            begin
+              SDL_DestroyWindow(window);
+              window := nil;
+            end;
+            if event.window.windowID = SDL_GetWindowID(window2) then
+            begin
+              SDL_DestroyWindow(window2);
+              window2 := nil;
+            end;
+          end;
+        end;
+      end;
     end;
   end;
 end;
 
-procedure Render;
+procedure Render(r,g,b: Integer);
 begin
   glClear( GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT );
 
-  glLoadIdentity();
+  glLoadIdentity;
   glTranslatef( -1.5, 0.0, -6.0 );
 
-  glColor3f(1,0,0);
+  glColor3f(r,g,b);
   glBegin( GL_TRIANGLES );
     glVertex3f( 0.0, 1.0, 0.0 );
     glVertex3f( -1.0, -1.0, 0.0 );
@@ -77,14 +86,13 @@ begin
 
   glTranslatef( 3.0, 0.0, 0.0 );
 
-  glColor3f(0,1,0);
+  glColor3f(r,g,b);
   glBegin( GL_QUADS );
     glVertex3f( -1.0, 1.0, 0.0 );
     glVertex3f( 1.0, 1.0, 0.0 );
     glVertex3f( 1.0, -1.0, 0.0 );
     glVertex3f( -1.0, -1.0, 0.0 );
   glEnd;
-  SDL_GL_SwapWindow(window);
 end;
 
 begin
@@ -94,14 +102,37 @@ begin
       raise Exception.Create('Error at initializing SDL_VIDEO: ' + SDL_GetError);
     end;
 
-    window := SDL_CreateWindow('Sample 1', 200, 200, 640, 400, SDL_WINDOW_OPENGL);
+    window := SDL_CreateWindow('Sample 2 - Window 1', 200, 200, 640, 400, SDL_WINDOW_OPENGL);
 
     if window = nil then
     begin
       raise Exception.Create('Error while creating Window: ' + SDL_GetError);
     end;
 
+    window2 := SDL_CreateWindow('Sample 2 - Window 2', 400, 400, 640, 400, SDL_WINDOW_OPENGL);
+
+    if window2 = nil then
+    begin
+      raise Exception.Create('Error while creating Window: ' + SDL_GetError);
+    end;
+
+    InitOpenGL;
+    ReadExtensions;
+    ReadImplementationProperties;
+
+    //Creates a context supporting opengl 1.0 and up.
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     context := SDL_GL_CreateContext(window);
+
+    if context = nil then
+    begin
+      raise Exception.Create('Error while creating Context: ' + SDL_GetError);
+    end;
 
     OpenGL;
     SetViewport(640,400);
@@ -110,11 +141,25 @@ begin
     while running do
     begin
       HandleEvents;
-      Render;
+      if window <> nil then
+      begin
+        SDL_GL_MakeCurrent(window, context);
+        Render(1,0,0);
+        SDL_GL_SwapWindow(window);
+      end;
+      if window2 <> nil then
+      begin
+        SDL_GL_MakeCurrent(window2, context);
+        Render(0,0,1);
+        SDL_GL_SwapWindow(window2);
+      end;
+      if (window = nil) and (window2 = nil) then
+        running := false;
     end;
 
-    SDL_DestroyWindow(window);
     SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(window2);
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
